13c7ed49bab5736eae8e68a013ed3f8f328997f1
Akylbek Kerimbekov <akyl.kerimbekov@gmail.com>
2023-11-06 12:46:50 +0100

    prepare

--- config/bot_config.py
+++ config/bot_config.py
@@ -2,11 +2,12 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from decouple import config
 
+PROXY_URL = config("PROXY_URL")
 TOKEN = config("TOKEN")
 ROOT_PATH = config("ROOT_PATH")
 DB_PATH = config("DB_PATH")
 DB_POOL_MAX_SIZE = int(config("DB_POOL_MAX_SIZE"))
 
 storage = MemoryStorage()
-bot = Bot(token=TOKEN)
+bot = Bot(token=TOKEN, proxy=PROXY_URL)
 dispatcher = Dispatcher(bot=bot, storage=storage)
